2f5cf91f649f895b10fc78bce124ba7422bfed29
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-11-21 11:50:16 -0600

    added more backend

---
+++ smtModels/GetCompanyByPriceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smtModels
+{
+    public class GetCompanyByPriceDto
+    {
+        public decimal? MaxPrice { get; set; }
+    }
+}
---
+++ smtModels/GetCompanyDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smtModels
+{
+    public class GetCompanyDto
+    {
+        public GetCompanyDto(string ticker)
+        {
+            Ticker = ticker;
+        }
+
+        public string Ticker { get; set; }    
+    }
+}
