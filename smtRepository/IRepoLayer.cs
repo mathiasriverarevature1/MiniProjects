35a51c6ee633c475dc587d118c21b08c0cd73bf8
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-11-29 13:44:02 -0600

    backend cont

--- smtRepository/IRepoLayer.cs
+++ smtRepository/IRepoLayer.cs
@@ -9,6 +9,9 @@ namespace smtRepository
 {
     public interface IRepoLayer
     {
-
+        Task<List<Stocks?>> GetAllCompaniesAsync();
+        Task<List<Stocks?>> GetCompanyByPriceAsync(GetCompanyByPriceDto price);
+        Task<Stocks?> GetCompanyByTickerAsync(GetCompanyDto ticker);
+        Task<Stocks?> UpdateCompanyAsync(updateDto update);
     }
 }
--- smtRepository/RepoLayer.cs
+++ smtRepository/RepoLayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,44 @@ namespace smtRepository
 {
     public class RepoLayer
     {
-        //Server=tcp:mathiasserverminiprojects.database.windows.net,1433;Initial Catalog=mathiasriveraSMT;Persist Security Info=False;User ID=mathias;Password=password123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+        //"Server=tcp:mathiasserverminiprojects.database.windows.net,1433;Initial Catalog=mathiasriveraSMT;Persist Security Info=False;User ID=mathias;Password=password123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public async Task<List<Stocks?>> GetAllCompaniesAsync()
+        {
+
+        }
+        public async Task<List<Stocks?>> GetCompanyByPriceAsync(GetCompanyByPriceDto price)
+        {
+
+        }
+        public async Task<Stocks?> GetCompanyByTickerAsync(GetCompanyDto ticker)
+        {
+
+            SqlConnection conn = new SqlConnection("Server=tcp:mathiasserverminiprojects.database.windows.net,1433;Initial Catalog=mathiasriveraSMT;Persist Security Info=False;User ID=mathias;Password=password123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            using (SqlCommand command = new SqlCommand($"SELECT FROM Stocks WHERE Ticker = @ticker", conn))
+            {
+                command.Parameters.AddWithValue("@ticker", ticker); //SQL inj prevention
+                conn.Open();
+                SqlDataReader? ret = await command.ExecuteReaderAsync();
+                while (ret.Read())
+                {
+                    Stocks s = new Stocks((Guid)ret[0], (Guid)ret[1], ret.GetString(2), ret.GetDecimal(3), ret.GetInt32(4));
+                }
+                conn.Close();
+                return s;
+            }
+
+        }
+        public async Task<Stocks?> UpdateCompanyAsync(updateDto update)
+        {
+
+        }
+
+
+
+
+
+
+
     }
 }
